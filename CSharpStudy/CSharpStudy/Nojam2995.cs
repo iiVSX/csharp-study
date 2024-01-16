83dd0a72b33702d8d8732cc441142ff8b01f92cf
iiVSX <iivsx@superplanet.net>
2024-01-16 23:49:11 +0900

    nojam 2995 with algorithm cumulative sum

---
+++ CSharpStudy/CSharpStudy/Nojam2995.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharpStudy
+{
+    class Nojam2559
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = Int32.Parse(input[0]);
+            int k = Int32.Parse(input[1]);
+
+            string[] arr = Console.ReadLine().Split();
+            int sum = 0;
+            for (int i = 0; i < k; i++)
+            {
+                sum += Int32.Parse(arr[i]);
+            }
+
+            int result = sum;
+            for (int i = k; i < n; i++)
+            {
+                sum = Int32.Parse(arr[i]) + sum - Int32.Parse(arr[i - k]);
+                result = Math.Max(result, sum);
+            }
+            
+            Console.WriteLine(result);
+        }
+    }    
+}
